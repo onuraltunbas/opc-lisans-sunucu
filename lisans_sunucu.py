482734149af97c653f29410ed45c46e3fc3045b8
onuraltunbas <onnuraltunbass@gmail.com>
2026-07-11 20:06:36 +0300

    cacheopt

--- lisans_sunucu.py
+++ lisans_sunucu.py
@@ -131,7 +131,7 @@ app.mount("/static", StaticFiles(directory=str(_static_dir)), name="static")
 _flappy_dir = pathlib.Path(__file__).parent / "flappyelectronics"
 _flappy_dir.mkdir(exist_ok=True)
 # html=True parametresi sayesinde /flappyelectronics yazılınca otomatik index.html açılır
-app.mount("/basurefe", StaticFiles(directory=str(_flappy_dir), html=True), name="basurefe")
+app.mount("/flappyelectronics", StaticFiles(directory=str(_flappy_dir), html=True), name="flappyelectronics")
 
 # =====================================================================
 # ROUTER KAYITLARI
